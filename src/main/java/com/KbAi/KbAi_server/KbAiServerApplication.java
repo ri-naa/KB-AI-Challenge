39e07c30301970913b6166631f0976c21069a3a2
ri-naa <rina288@ewhain.net>
2025-08-05 16:42:31 +0900

    initial commit

---
+++ src/main/java/com/KbAi/KbAi_server/KbAiServerApplication.java
@@ -0,0 +1,13 @@
+package com.KbAi.KbAi_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbAiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbAiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KbAi/KbAi_server/KbAiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KbAi.KbAi_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbAiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
